6123ad4a4283099b9e07a253097499ee980237de
Bruno Moleta <brunomoleta@pm.me>
2024-05-06 11:27:41 -0300

    :recycle: refatoração da função
    Agora inclui mensagens de erro caso não seja inserida uma função válida.

--- m-20/2-highIndex.js
+++ m-20/2-highIndex.js
@@ -1,11 +1,19 @@
 const highLowIndex = (arr) => {
-    const higher = Math.max(...arr);
-    const lower = Math.min(...arr);
+    const errorMessage = "Por favor insira um array com um número ou mais."
+    if (!arr) return errorMessage;
+    if (!arr.some(Number.isFinite)) return errorMessage;
 
-    const minIndex = arr.indexOf(lower)
-    const maxIndex = arr.indexOf(higher)
+    const successMessage = (lower, minIndex, higher, maxIndex) => `O menor número é ${lower} e o índice dele é ${minIndex}, enquanto o maior número é ${higher} e o índice dele é ${maxIndex}`
 
-    return `O menor número é ${lower} e o índice dele é ${minIndex}, enquanto o maior número é ${higher} e o índice dele é ${maxIndex}`;
+    const numbersArr = arr.filter(Number.isFinite)
+    const higher = Math.max(...numbersArr);
+    const lower = Math.min(...numbersArr);
+
+    const minIndex = arr.indexOf(lower);
+    const maxIndex = arr.indexOf(higher);
+
+
+    return successMessage(lower, minIndex, higher, maxIndex);
 }
 
-console.log(highLowIndex([12, 7, 34, 45, 78, 22, 9, 67, 83, 31]))
+module.exports = highLowIndex
